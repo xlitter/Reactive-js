0a516da078e406fbe67de26099ef895a84635c8d
wei.he <9638527_4_1@163.com>
2015-12-03 16:56:23 +0800

    modify list register click pattern

--- static/scripts/unit2/1.js
+++ static/scripts/unit2/1.js
@@ -7,9 +7,11 @@
 		url = 'http://localhost:3000/list',
 		// url = 'https://api.github.com/users',
 		refreshClickStream = Rx.Observable.fromEvent($refresh, 'click'),
-		listCloseClickStream = Rx.Observable.fromEvent($list, 'click', e => {
-			return e.target;
-		}).filter(element=> element.className.indexOf('close') > -1),
+		listCloseClickStream = Rx.Observable.fromEventPattern((h) => {
+			$list.on('click', '.close', h);
+		}, (h) => {
+			$list.off('click', '.close', h);
+		}),
 		reqStream = refreshClickStream
 		//触发click事件,只对fromEvent内的click事件有效
 			.startWith('startup click')
@@ -29,7 +31,7 @@
 		//使其在点击close按钮和rspSream发生变化时都可以触发(click, list)=>{}方法的执行
 			.combineLatest(rspStream, (...rest) => {
 				let list = rest[1];
-				return  list[Math.floor(Math.random() * list.length)] ;
+				return list[Math.floor(Math.random() * list.length)];
 			})
 		//合并refreshClickSream,使其点击refresh时先触发subscribe,传入null值
 			.merge(refreshClickStream.map(() => null))
@@ -41,7 +43,7 @@
 		return `<div class="item">
 						<img class="avatar" src="${v.avatar_url}" alt="${v.login}">
 						<span class="name">${v.login}</span>
-						<i class="close close1">x</i>
+						<i class="close">x</i>
 						</div>`;
 	}
 
@@ -49,10 +51,10 @@
 		// .filter((...rest) => {
 		// 	console.log('rest', rest);
 		// });
-	.subscribe(v => {
-		//jshint -W030
-		 v&&$list.empty().append(createElement(v))||$list.empty();
-	});
+		.subscribe(v => {
+			//jshint -W030
+			v && $list.empty().append(createElement(v)) || $list.empty();
+		});
 
 		// rspStream.subscribe((rsp) => {
 		// $list.empty().append(rsp.map(v=> `<div class="item">
